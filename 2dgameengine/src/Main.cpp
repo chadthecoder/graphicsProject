c0c306a66d0be19c1a9ec1e14524bb3a26b87f03
chadthecoder <mchadamschad@outlook.com>
2019-11-18 07:08:32 -0800

    deleted cyborg udemy project, added new cpp and lua udemy project, finished image test which works on linux, learn how to compile windows binaries on linux

---
+++ 2dgameengine/src/Main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char *args[])
+{
+    std::cout << "Game is running." << std::endl;
+    return 0;
+}
---
+++ imageTest/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+#include "libsWin\glew\include\GL\glew.h"
+#include "libsWin\glfw\include\GLFW\glfw3.h"
+#include "libsWin\SDL2\i686-w64-mingw32\include\SDL2\SDL.h"
+#include "libsWin\SDL2_image-2.0.3\i686-w64-mingw32\include\SDL2\SDL_image.h"
+#else
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#endif
+
+int main(int argc, char **argv)
+{
+	bool quit = false;
+	SDL_Event event;
+
+	SDL_Init(SDL_INIT_VIDEO);
+	IMG_Init(IMG_INIT_JPG);
+
+	SDL_Window *window = SDL_CreateWindow("SDL2 Displaying Image",
+																				SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+	SDL_Surface *image = IMG_Load("plane.jpg");
+
+	SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, image);
+
+	while (!quit)
+	{
+		SDL_WaitEvent(&event);
+
+		switch (event.type)
+		{
+		case SDL_QUIT:
+			quit = true;
+			break;
+		}
+
+		//SDL_Rect dstrect = { 5, 5, 320, 240 };
+		//SDL_RenderCopy(renderer, texture, NULL, &dstrect);
+		SDL_RenderCopy(renderer, texture, NULL, NULL);
+		SDL_RenderPresent(renderer);
+	}
+
+	SDL_DestroyTexture(texture);
+	SDL_FreeSurface(image);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+
+	IMG_Quit();
+	SDL_Quit();
+
+	return 0;
+}
