88a3e3988a7e861c51a5264c0267ae1cadfe0cf6
chadthecoder <mcadamschad@outlook.com>
2023-09-03 10:15:46 -0700

    changed client to standalone asio, working on server, but is in comments for now because it wasnt working, fixed naming issue causing day string to try to be put in one char

--- asioGraphicsNetwork/client.cpp
+++ asioGraphicsNetwork/client.cpp
@@ -9,10 +9,15 @@
 //
 
 #include <iostream>
-#include <boost/array.hpp>
-#include <boost/asio.hpp>
+// #include <boost/array.hpp>
+// #include <boost/asio.hpp>
+// #define ASIO_STANDALONE
+#include "asio-1.28.0/include/asio.hpp"
+#include "asio-1.28.0/include/asio/ts/socket.hpp"
+#include <asio.hpp>
+#include <asio/ts/buffer.hpp>
 
-using boost::asio::ip::udp;
+// using asio::ip::udp;
 
 int main(int argc, char *argv[])
 {
@@ -24,27 +29,34 @@ int main(int argc, char *argv[])
             return 1;
         }
 
-        boost::asio::io_context io_context;
+        asio::io_context io_context;
 
-        udp::resolver resolver(io_context);
-        udp::endpoint receiver_endpoint =
-            *resolver.resolve(udp::v4(), argv[1], "daytime").begin();
+        asio::ip::udp::resolver resolver(io_context);
+        asio::ip::udp::endpoint receiver_endpoint =
+            *resolver.resolve(asio::ip::udp::v4(), argv[1], "1024").begin();
 
-        udp::socket socket(io_context);
-        socket.open(udp::v4());
+        asio::ip::udp::socket socket(io_context);
+        socket.open(asio::ip::udp::v4());
 
-        boost::array<char, 1> send_buf = {{0}};
-        socket.send_to(boost::asio::buffer(send_buf), receiver_endpoint);
+        size_t bytes = socket.available();
+        char data[1];
+        // std::vector<char> vBuff(bytes);
+        //  boost::array<char, 1> send_buf = {{0}};
+        socket.send_to(asio::buffer(data), receiver_endpoint);
 
         // while (true)
         //{
 
-        boost::array<char, 128> recv_buf;
-        udp::endpoint sender_endpoint;
+        size_t bytes2 = socket.available();
+        char data2[128];
+        // std::vector<char, 128> vBuff2();
+        //  boost::array<char, 128> recv_buf;
+        asio::ip::udp::endpoint sender_endpoint =
+            *resolver.resolve(asio::ip::udp::v4(), argv[1], "1024").begin();
         size_t len = socket.receive_from(
-            boost::asio::buffer(recv_buf), sender_endpoint);
+            asio::buffer(data2), sender_endpoint);
 
-        std::cout.write(recv_buf.data(), len);
+        std::cout.write(data2, len);
         //}
     }
     catch (std::exception &e)
--- asioGraphicsNetwork/server.cpp
+++ asioGraphicsNetwork/server.cpp
@@ -30,7 +30,7 @@ class udp_server
 {
 public:
     udp_server(asio::io_context &io_context)
-        : socket_(io_context, udp::endpoint(udp::v4(), 13))
+        : socket_(io_context, udp::endpoint(udp::v4(), 1024))
     {
         start_receive();
     }
@@ -57,12 +57,13 @@ private:
     void handle_receive(const asio::error_code &error,
                         std::size_t /*bytes_transferred*/)
     {
-        std::cout << "handle_receive" << std::endl;
+        std::cout << "handle_receive " << error << std::endl;
 
         if (!error)
         {
             day = make_daytime_string();
             sizeDay = day.size();
+            std::cout << day << " " << sizeDay << std::endl;
             boost::shared_ptr<std::string> message(
                 new std::string(day));
 
@@ -108,3 +109,130 @@ int main()
 
     return 0;
 }
+
+//
+// server.cpp
+// ~~~~~~~~~~
+//
+// Copyright (c) 2003-2023 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+/* #include <ctime>
+#include <iostream>
+#include <string>
+#include <boost/array.hpp>
+#include <boost/bind/bind.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/system/error_code.hpp>
+#include <asio.hpp>
+
+using asio::ip::udp;
+
+std::string make_daytime_string()
+{
+    using namespace std; // For time_t, time and ctime;
+    time_t now = time(0);
+    return ctime(&now);
+}
+
+class udp_server
+{
+public:
+    udp_server(asio::io_context &io_context)
+        : socket_(io_context, udp::endpoint(udp::v4(), 1024))
+    {
+        start_receive();
+    }
+
+private:
+    // start waiting for client to send
+    void start_receive()
+    {
+        // other option to pass instead of asio::use_future
+        //  asio::yield_context yield;
+
+        // if implements this instead must call handle_receive separately, can do for send as well
+        // std::future<std::size_t> my_future = socket_.async_receive_from(asio::buffer(recv_buffer_), remote_endpoint_, asio::use_future);
+        // handle_receive(iError1, recvSize); */
+
+/*         socket_.async_receive_from(
+            asio::buffer(recv_buffer_), remote_endpoint_,
+            boost::bind(&udp_server::handle_receive, this,
+                        iError1,
+                        recvSize)); */
+/*std::future<std::size_t> my_future1 = socket_.async_receive_from(
+    asio::buffer(recv_buffer_), remote_endpoint_,
+    asio::use_future);
+std::cout << "hi1" << std::endl;
+my_future1.wait();
+std::cout << "hi " << my_future1.get() << std::endl;
+handle_receive(iError1, recvSize);
+} */
+
+// to do after recieved from client, send info back
+/* void handle_receive(const asio::error_code &error,
+                    std::size_t */
+/*bytes_transferred*/ /* )
+{
+std::cout << "handle_receive" << std::endl;
+
+if (!error)
+{
+day = make_daytime_string();
+// std::cout << day << std::endl;
+sizeDay = day.size();
+boost::shared_ptr<std::string> message(
+new std::string(day));
+
+socket_.async_send_to(asio::buffer(*message), remote_endpoint_,
+boost::bind(&udp_server::handle_send, this, message,
+       iError2,
+       sizeDay));
+
+// start listening for another client send
+start_receive();
+}
+}
+
+// what to do with info recieved?
+void handle_send(boost::shared_ptr<std::string> */
+/*message*/           /*,
+const asio::error_code & */
+/*error*/             /*,
+std::size_t */
+/*bytes_transferred*/ /* )
+{
+}
+
+udp::socket socket_;
+udp::endpoint remote_endpoint_;
+boost::array<char, 1> recv_buffer_;
+std::string day;
+std::size_t recvSize = recv_buffer_.size(), sizeDay;
+boost::system::error_code iError1, iError2;
+}
+;
+
+int main()
+{
+try
+{
+asio::io_context io_context;
+udp_server server(io_context);
+io_context.run();
+asio::steady_timer t(io_context, asio::chrono::seconds(5));
+*/
+                      //  t.wait();
+                      /*
+                      }
+                      catch (std::exception &e)
+                      {
+                          std::cerr << e.what() << std::endl;
+                      }
+                      
+                      return 0;
+                      }
+                      */
