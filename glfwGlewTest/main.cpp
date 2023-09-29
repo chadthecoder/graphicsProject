920596a2e985aa37a329b95aca22699a6425b512
chadthecoder <mcadamschad@outlook.com>
2023-09-28 22:57:50 -0700

    fixed bug following cherno vid

--- glfwGlewTest/main.cpp
+++ glfwGlewTest/main.cpp
@@ -219,7 +219,7 @@ int main(void)
     unsigned int buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
